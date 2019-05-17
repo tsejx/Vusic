e905c2c4d22f920e3208fb3c4129c3153eeaecf0
tsejx <tsejx@foxmail.com>
2019-05-17 16:07:06 +0800

    :tada: Initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    Hello world!
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+import MHeader from 'components/m-header'
+export default {
+  name: 'App',
+  components: {
+    MHeader
+  }
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  // @import 'common/stylus/variable'
+</style>
---
+++ src/components/m-header/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        Hello world!
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'm-header'
+}
+</script>
+
+<style>
+
+</style>
+
