942568567dc972df3ec8234bf04d39b6d8987964
tsejx <tsejx@foxmail.com>
2019-07-18 17:12:49 +0800

    :bug: fix:fix bug that no lyric when radom play favorite list and histroy list

--- src/main.js
+++ src/main.js
@@ -2,10 +2,13 @@ import 'babel-polyfill';
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
-import store from './store'
+import store from './store';
 // 解决移动端 300ms 延迟
 import fastclick from 'fastclick';
 import VueLazyload from 'vue-lazyload';
+import { SET_PLAY_HISTORY, SET_FAVORITE_LIST } from './store/mutation-types';
+import { loadPlay, loadFavorite } from 'common/js/cache';
+import { injectSongUrl } from 'common/js/song';
 
 import 'styles/index.styl';
 
@@ -15,6 +18,16 @@ fastclick.attach(document.body);
 
 Vue.use(VueLazyload, {
   loading: require('assets/images/default.png'),
+});
+
+const historySongs = loadPlay();
+injectSongUrl(historySongs).then(songs => {
+  store.commit(SET_PLAY_HISTORY, songs);
+});
+
+const favoriteSongs = loadFavorite();
+injectSongUrl(favoriteSongs).then(songs => {
+  store.commit(SET_FAVORITE_LIST, songs);
 });
 
 /* eslint-disable no-new */
--- src/store/state.js
+++ src/store/state.js
@@ -1,31 +1,31 @@
-import { playMode } from 'common/js/config'
-import { loadSearchHistroy, loadPlay, loadFavorite } from 'common/js/cache'
+import { playMode } from 'common/js/config';
+import { loadSearchHistroy } from 'common/js/cache';
 
 const state = {
-    // 最近查看歌手信息
-    singer: {},
-    // 歌曲播放状态
-    playing: false,
-    // 播放器全屏
-    isFullScreen: false,
-    // 播放列表
-    playlist: [],
-    // 播放序列
-    sequenceList: [],
-    // 播放模式
-    mode: playMode.sequence,
-    // 当前播放歌曲索引
-    currentIndex: -1,
-    // 歌单列表
-    disclist: [],
-    // 榜单列表
-    ranklist: [],
-    // 搜索历史列表
-    searchHistory: loadSearchHistroy(),
-    // 播放历史
-    playHistory: loadPlay(),
-    // 收藏列表
-    favoriteList: loadFavorite(),
-}
+  // 最近查看歌手信息
+  singer: {},
+  // 歌曲播放状态
+  playing: false,
+  // 播放器全屏
+  isFullScreen: false,
+  // 播放列表
+  playlist: [],
+  // 播放序列
+  sequenceList: [],
+  // 播放模式
+  mode: playMode.sequence,
+  // 当前播放歌曲索引
+  currentIndex: -1,
+  // 歌单列表
+  disclist: [],
+  // 榜单列表
+  ranklist: [],
+  // 搜索历史列表
+  searchHistory: loadSearchHistroy(),
+  // 播放历史
+  playHistory: [],
+  // 收藏列表
+  favoriteList: [],
+};
 
-export default state
+export default state;
